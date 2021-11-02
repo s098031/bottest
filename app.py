fbbe99b6e687b269ebd98b5925744c7fc6a18a6d
s098031 <68554490+s098031@users.noreply.github.com>
2021-11-02 17:26:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 from datetime import datetime
 #============================================================
-from flask import Flask, request, abort
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import *
+from flask import Flask, abort, request
+# https://github.com/line/line-bot-sdk-python
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 #============================================================
 from Index import *
 from Animation import *
@@ -12,39 +13,26 @@ from FlexMessage import *
 import Globals
 #============================================================
 
-# https://github.com/line/line-bot-sdk-python
+
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
+#============================================================
 Globals
-
-@app.route("/", methods=["GET", "POST"])
+#============================================================
+@app.route("/callback", methods=['POST'])
 def callback():
-
-    if request.method == "GET":
-        return "Hello Heroku"
-    if request.method == "POST":
-        signature = request.headers["X-Line-Signature"]
-        body = request.get_data(as_text=True)
-
-        try:
-            handler.handle(body, signature)
-        except InvalidSignatureError:
-            abort(400)
-
-        return "OK"
-#=============================================================
-# echo
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    get_message = event.message.text
-
-    # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 #=============================================================
 # welcome
 @handler.add(JoinEvent)
