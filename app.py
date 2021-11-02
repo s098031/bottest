ec513b1bb27b4198dc73b3cd4b114d3f37f1a118
s098031 <68554490+s098031@users.noreply.github.com>
2021-11-02 17:08:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,11 +36,5 @@ def callback():
 def handle_message(event):
     get_message = event.message.text
 
-    # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
+
 
